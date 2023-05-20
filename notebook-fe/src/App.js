73fcd09a7ce6725634dd8fa0167278ff4eb29bad
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-20 21:20:14 +0530

    added add note section

--- notebook-fe/src/App.js
+++ notebook-fe/src/App.js
@@ -3,17 +3,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/NavBar';
 import Home from './Components/Home';
 import Aboutus from './Components/AboutUs';
-
+import Notes from './Components/Notes';
+import { createContext, useState } from 'react';
+export const userContext = createContext(null)
 function App() {
+  const [userToken, setUserToken] = useState("")
+  const userContextValue = { userToken: "eyJhbGciOiJIUzI1NiJ9.NjQ1YmRjNjRjMmU3NDBkNjg0ZWNiNjhh.y-aPHDkV7t1hm9KVv4UVUwKvENAOvvp7drlcwaifESY", setUserToken };
+
   return (
-    <BrowserRouter>
-      <Navbar title="NoteBook"></Navbar>
+    <userContext.Provider value={userContextValue}>
+      <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<Aboutus />} />
+          <Route path='/home' element={<><Navbar title="NoteBook"></Navbar> <Home /></>} />
+          <Route path='/about' element={<><Navbar title="NoteBook"></Navbar><Aboutus /></>} />
+          <Route path='/your-notes' element={<><Navbar title="NoteBook"></Navbar><Notes /></>} />
       </Routes>
     </BrowserRouter>
+    </userContext.Provider>
   );
 }
 
 export default App;
+
---
+++ notebook-fe/src/Components/Notes.js
@@ -0,0 +1,6 @@
+function Notes() {
+    return (
+        <h1>This Is Your Notes</h1>
+    )
+}
+export default Notes
