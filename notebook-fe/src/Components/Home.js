d90e20a88f31fe41d24114b9ea74204ee37cd5c9
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-20 13:42:24 +0530

    added use location and added add note form

--- notebook-fe/src/Components/Home.js
+++ notebook-fe/src/Components/Home.js
@@ -1,6 +1,31 @@
+
+import '../App.css'
 function Home() {
     return (
-        <h1>This is home</h1>
+        <div className="container">
+
+
+
+
+
+            <div class="bg text-center">
+
+
+                <div class="centered">
+
+                    <p class="firstLine"> A &nbsp; D &nbsp; D &nbsp; N &nbsp; O &nbsp; T &nbsp; E &nbsp; S </p>
+
+                    <p class="secondLine">CLICK BELOW TO ADD YOUR NOTES</p>
+
+                    <p> <input class="InputStyle"
+                        style={{ "font-family": "Arial" }} type="text" /> </p>
+                    <button type="button" class="btn btn-primary my-10">Submit</button>
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
 export default Home
